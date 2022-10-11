fa1e082732d28fd667a40d4cdf50f6dbdab9cf5a
Roman <romankos1993@gmail.com>
2022-10-11 21:10:20 +0300

    task 002 completed

---
+++ task002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+// PositiveNumber(num1);
+Console.WriteLine(NumDigits(num1));
+
+// // int PositiveNumber(int number)
+// {
+//     if (number < 0)
+//     {
+//         number *= -1;
+//     }
+// }
+int NumDigits(int number)
+{
+    int count = 0;
+    while (number > 1)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
