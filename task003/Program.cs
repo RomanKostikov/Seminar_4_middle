7e4d84dd4cc27954858539239b54cdee3b9251a2
Roman <romankos1993@gmail.com>
2022-10-11 21:11:10 +0300

    task 003 completed

---
+++ task003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+Console.Clear();
+Console.Write("Print an integer: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumsProd(num1));
+
+int NumsProd(int number)
+{
+    int Prod = 1;
+    while (number > 1)
+    {
+        Prod = Prod * number;
+        number--;
+    }
+    return Prod;
+}
